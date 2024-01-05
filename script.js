5e272ebbe1ac9a408d2a737a7dbe2688493124d6
apoloreyes28 <reyesapolinar31@gmail.com>
2024-01-05 14:02:26 -0700

    Starbucks landing page made for educational and demonstration purposes only

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;// we change the attribute src
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
